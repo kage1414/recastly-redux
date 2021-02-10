ad17c80487aa6b85b301c9fb664c1dc900959c0a
Frederic Rosselet <fred.rosselet@gmail.com>
2021-02-09 20:08:57 -0800

    Complete planning state

--- src/actions/search.js
+++ src/actions/search.js
@@ -17,28 +17,6 @@ var handleVideoSearch = (q) => {
       dispatch(changeVideo(data[0]));
     });
   };
-  //TODO:  Write an asynchronous action to handle a video search!
 };
 
 export default handleVideoSearch;
-
-// var searchYouTube = ({key, query, max = 5}, callback) => {
-//   $.get('https://www.googleapis.com/youtube/v3/search', {
-//     part: 'snippet',
-//     key: key,
-//     q: query,
-//     maxResults: max,
-//     type: 'video',
-//     videoEmbeddable: 'true'
-//   })
-//     .done(({items}) => {
-//       if (callback) {
-//         callback(items);
-//       }
-//     })
-//     .fail(({responseJSON}) => {
-//       responseJSON.error.errors.forEach((err) =>
-//         console.error(err)
-//       );
-//     });
-// };
--- src/store/store.js
+++ src/store/store.js
@@ -5,3 +5,12 @@ import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
 
+let initialState = {
+  videos: exampleVideoData,
+  currentVideo: exampleVideoData[0],
+  searchValue: ''
+};
+
+let enhancer = applyMiddleware(thunk);
+
+const store = createStore(rootReducer, initialState, enhancer);
